bd8977455ed59d111c67b2892934d3304b3c8f75
Omar Cortassa <omar.cortassa@gmail.com>
2019-10-30 15:59:07 +0100

    qa

--- labs/15-Memory_limits/app/MaxMemory.java
+++ labs/15-Memory_limits/app/MaxMemory.java
@@ -1,8 +1,9 @@
 
 package app;
 
-public class MaxMemory{
-    public static void main(String[] args){
+public class MaxMemory {
+    public static void main(String[] args) {
         System.out.println("Max Memory: "+Runtime.getRuntime().maxMemory() / 1024 / 1024);
     }
+    
 }
